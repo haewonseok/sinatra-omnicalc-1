992236cf3f3c372a34057c14106120eb1a690bf3
Haewon Seok <114521565+haewonseok@users.noreply.github.com>
2024-04-06 02:41:36 +0000

    Square Completed

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_new)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+  
+  erb(:square_results)
 end
