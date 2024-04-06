54241a2c226257d9ee6ab80648e84dea447df46c
Haewon Seok <114521565+haewonseok@users.noreply.github.com>
2024-04-06 02:47:22 +0000

    Square Root Completed

--- app.rb
+++ app.rb
@@ -16,3 +16,15 @@ get("/square/results") do
   
   erb(:square_results)
 end
+
+get("/square_root/new") do
+  erb(:square_root_new)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = Math.sqrt(@the_num)
+  
+  erb(:square_root_results)
+end
